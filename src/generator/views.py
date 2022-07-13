d5622f20450170fa51dbfcb4e5cfda4686e04407
KM <tsnok777@gmail.com>
2022-07-13 13:10:33 +0300

    add page eggs

--- src/generator/views.py
+++ src/generator/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Hello there friend")
+
+def eggs(request):
+    return HttpResponse("this page eggs")
