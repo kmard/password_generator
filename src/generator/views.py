af8cf0ee5cd4a36fd873560b3eec35199ec6a056
KM <tsnok777@gmail.com>
2022-07-13 13:22:50 +0300

    out put test password on home page

--- src/generator/views.py
+++ src/generator/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request,'generator/home.html')
+    return render(request,'generator/home.html',{'password':'HJyyuykl124'})
 
 def eggs(request):
     return HttpResponse("<h1>this page eggs</h1>")
