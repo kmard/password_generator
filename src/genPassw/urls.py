80f8f165d0254ac3da7c99185800b6f20e4cf323
KM <tsnok777@gmail.com>
2022-07-13 14:13:22 +0300

    generate simple passwords

--- src/genPassw/urls.py
+++ src/genPassw/urls.py
@@ -20,5 +20,5 @@ from generator import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
-    path('generatedpassword/',views.password,name='password'),
+    path('password/',views.password,name='password'),
 ]
